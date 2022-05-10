920298b4d73a30fe6eb0420ff253ec3d815de0b4
JM-Cheon <sky4jm@gmail.com>
2022-05-10 22:18:33 +0900

    Test:express test

--- src/server.js
+++ src/server.js
@@ -1,3 +1,19 @@
 import express from "express";
 
+const PORT = 4000;
+
 const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("<h1>Test Express!</h1>");
+};
+const handleLogin = (req, res) => {
+  return res.send("<h1>Login here.</h1>");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () => console.log("Server litening on port 4000");
+
+app.listen(PORT, handleListening);
