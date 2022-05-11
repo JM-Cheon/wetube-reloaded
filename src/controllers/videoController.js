f4ef981c1c864acd3a6c22add49efc4fefcb1b8a
JM-Cheon <sky4jm@gmail.com>
2022-05-11 18:02:40 +0900

    Feat:Add model, init.js, db.js

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,18 +1,31 @@
-const fakeUser = {
-  username: "JM",
-  loggedIn: true,
-};
+import Video from "../models/Video";
 
 // global
-export const home = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
-export const search = (req, res) => res.send("Search Video");
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {
+    if (error) console.log(error);
+    console.log(videos);
+  });
+  return res.render("home", { pageTitle: "Home", videos: [] });
+};
 
 // videos
-export const upload = (req, res) => res.send("Upload Video");
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const remove = (req, res) => {
-  console.log(req.params);
-  res.send("Remove Video");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  return res.render("edit", { pageTitle: `Editing ${video.title}` });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  return res.redirect(`/videos/${id}`);
+};
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  return res.redirect("/");
 };
