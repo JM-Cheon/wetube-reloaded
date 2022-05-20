0f071316bdaab9b38d316b1be3ae2d502c82170f
JM-Cheon <sky4jm@gmail.com>
2022-05-20 17:45:29 +0900

    Fixed: userController fix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,44 @@
+import User from "../models/User";
+
 // global
-export const join = (req, res) => res.send("Join");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password1, password2, location } = req.body;
+  const pageTitle = "Join";
+
+  if (password1 !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is aleady taken.",
+    });
+  }
+
+  try {
+    await User.create({
+      name,
+      email,
+      username,
+      password1,
+      location,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+
+  res.redirect("/login");
+};
 export const login = (req, res) => res.send("Login");
 
 // users
